79e817ee498e0f5529c7e0a4d5b334bee0b30f58
Orso Meneghini <orso82@gmail.com>
2024-10-25 12:10:56 -0700

    add analytics

--- docs/make.jl
+++ docs/make.jl
@@ -18,7 +18,7 @@ end
 
 makedocs(;
     modules=[CoordinateConventions],
-    format=Documenter.HTML(),
+    format=Documenter.HTML(;analytics="G-65D8V8C8VQ"),
     sitename="CoordinateConventions",
     checkdocs=:none,
     pages=["index.md", "api.md", "License" => "license.md", "Notice" => "notice.md"]
